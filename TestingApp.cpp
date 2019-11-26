643d0a1f6e47c3d165c1f8d672035acd2c64b827
John Dinh <johnduydinh@gmail.com>
2019-11-26 11:53:32 -0500

    this is the answer

--- TestingApp.cpp
+++ TestingApp.cpp
@@ -1,22 +1,9 @@
-#include <iostream>
-#include <array>
-
-using namespace std;
-int main()
-{
-	int statues[] = { 6, 2, 3, 8 };
-	int size = sizeof(statues) / sizeof(statues[0]);
-	int missing = 0;
-	int total = 0;
-	sort(statues, statues + size); // {2,3,6,8}
-
-	for (int i = 0; i < size; i++) {
-		if (statues[i + 1] - statues[i] > 1) {
-			missing = (statues[i + 1] - statues[i]) - 1;
-			total = total + missing;
-		}
-	}
-	cout << total;
-
-	return 0;
+int makeArrayConsecutive2(std::vector<int> statues) {
+    std::sort(statues.begin(), statues.end());
+    int missing=0;
+    for(size_t i =1; i < statues.size(); ++i)
+    {
+        missing+=statues[i]-statues[i-1]-1;
+    }
+    return missing;
 }
